fe3eec2583468409876fe3c38a989334cadb7389
Yashraj Desai <yashrajdesai30@gmail.com>
2022-03-28 09:55:58 +0530

    docs added

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
-
+//Database configuration
 require('./configs/mongodb.config');
 
 //Route constants
 const productRoute = require('./routes/product.route');
 const userRoute = require('./routes/user.route');
 const orderRoute = require('./routes/order.route');
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+
+const swaggerOptions = {
+    definition: {
+        info: {
+            title: 'API-101',
+        },
+    },
+    apis: ['index.js', './documentation/*.yml'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //middlewares
 app.use(express.json());
